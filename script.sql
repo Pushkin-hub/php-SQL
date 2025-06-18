1c10d1bd3f4a462ab23e0100db7ec8f452f7fc3f
Pushkin Anton <pushkinanton2002@gmail.com>
2025-06-18 21:09:25 +0300

    first commit

--- script.sql
+++ script.sql
@@ -1,6 +1,12 @@
 ATTACH DATABASE 'database.sqlite';
 
 CREATE TABLE IF NOT EXISTS 'Students' (
-    id INTERGER PRIMARY KEY AUTOINCREMENT,
-    full_name 
-)
+id INT PRIMARY KEY AUTOINCREMENT,
+full_name TEXT NOT NULL,
+sity TEXT NOT NULL);
+
+INSERT INTO Students (full_name, city) VALUES ('Петр Петрович Петров', 'Краснодар');
+
+INSERT INTO Students (full_name, city) VALUES ('Гадя Петрович Хреново', 'Гадюкина');
+
+SELECT * FROM Students;
